3c552f56d342143deffe66594ccf11d25cf49a1a
Jan Kaltofen <Jan@Kaltofen.eu>
2018-04-24 15:56:50 +0200

    adapted to generate multiple columns of fire (2D-flame)

--- TorchFire.ino
+++ TorchFire.ino
@@ -1,9 +1,14 @@
 #include <FastLED.h>
 
+#define NUM_COLS 5
+#define NUM_ROWS 10
+
+#define MAX_IGNITE_HIGH 4
+
 #define LED_PIN     5
 #define COLOR_ORDER GRB
 #define CHIPSET     WS2811
-#define NUM_LEDS    30
+#define NUM_LEDS    NUM_COLS*NUM_ROWS
 
 #define BRIGHTNESS  200
 #define FRAMES_PER_SECOND 60
@@ -68,38 +73,49 @@ void loop()
 // Default 120, suggested range 50-200.
 #define SPARKING 120
 
+// returns the idx in the flat heat-array based on the given column and row.
+inline unsigned int GetHeatIdx(int col, int row)
+{
+  // LEDs are arranged from bottom to the top row by row.
+  // (therefore columns and rows are interchanged)
+  return row*NUM_COLS + col;
+}
 
 void Fire2012()
 {
-// Array of temperature readings at each simulation cell
+  // Array of temperature readings at each simulation cell
   static byte heat[NUM_LEDS];
 
   // Step 1.  Cool down every cell a little
-    for( int i = 0; i < NUM_LEDS; i++) {
-      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
-    }
+  for( int i = 0; i < NUM_LEDS; i++) {
+    heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+  }
   
-    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-    for( int k= NUM_LEDS - 1; k >= 2; k--) {
-      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+  for(int col = 0; col < NUM_COLS; col++) {
+    for( int row = NUM_ROWS - 1; row >= 2; row--) {
+      heat[GetHeatIdx(col, row)] = (heat[GetHeatIdx(col, row - 1)] + heat[GetHeatIdx(col, row - 2)] + heat[GetHeatIdx(col, row - 2)]) / 3;
     }
-    
-    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+  }
+  
+  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+  for(int col = 0; col < NUM_COLS; col++) {
     if( random8() < SPARKING ) {
-      int y = random8(7);
+      int y = GetHeatIdx(col, random8(MAX_IGNITE_HIGH));
       heat[y] = qadd8( heat[y], random8(160,255) );
     }
+  }
 
-    // Step 4.  Map from heat cells to LED colors
-    for( int j = 0; j < NUM_LEDS; j++) {
-      CRGB color = HeatColor( heat[j]);
-      int pixelnumber;
-      if( gReverseDirection ) {
-        pixelnumber = (NUM_LEDS-1) - j;
-      } else {
-        pixelnumber = j;
-      }
-      leds[pixelnumber] = color;
+  // Step 4.  Map from heat cells to LED colors
+  for( int j = 0; j < NUM_LEDS; j++) {
+    CRGB color = HeatColor( heat[j]);
+    int pixelnumber;
+    if( gReverseDirection ) {
+      pixelnumber = (NUM_LEDS-1) - j;
+    } else {
+      pixelnumber = j;
     }
+    leds[pixelnumber] = color;
+  }
 }
 
